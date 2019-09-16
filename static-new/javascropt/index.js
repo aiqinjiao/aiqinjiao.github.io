295bc46b9f8ee2d894cf335652d899eb8b53569b
aiqinjiao <mshenko@163.com>
2019-09-16 18:01:55 +0800

    更新首页

---
+++ static-new/javascropt/index.js
@@ -0,0 +1,27 @@
+let projects = [{
+        name: 'guess game',
+        url: 'project/number-guessing-game.html'
+    },
+    {
+        name: 'timekeeper',
+        url: 'project/timekeeper.html'
+    },
+    {
+        name: 'timepiece',
+        url: 'project/timepiece.html'
+    },
+    {
+        name: 'more...',
+        url: '#'
+    }
+]
+
+let proCount = projects.length;
+
+const proUl = document.querySelector('.project ul');
+
+for (let i = 0; i <= proCount; i++) {
+    let proUlLi = document.createElement('li');
+    proUlLi.innerHTML = '<a href="' + projects[i]["url"] + '" target="_blank">' + projects[i]['name'] + '</a>';
+    proUl.appendChild(proUlLi);
+}
